d9455bc1edb4af68349576da180695f670e4417c
Andy <coding@andysmiles4games.com>
2014-12-28 12:20:01 +0100

    Source Code Maintenance 	- [CHANGE] replaced tabs with spaces

--- Prototypes/VoxelTerrainDemo/Src/DemoScene.h
+++ Prototypes/VoxelTerrainDemo/Src/DemoScene.h
@@ -1,6 +1,6 @@
 /**
- * @version     1.3.0 04-Dec-14
- * @copyright   Copyright (c) 2014 by Andy Liebke. All rights reserved. 
+ * @version		1.3.0 04-Dec-14
+ * @copyright	Copyright (c) 2014 by Andy Liebke. All rights reserved. 
  */
 #ifndef __DEMO_SCENE_H__
 #define __DEMO_SCENE_H__
@@ -10,23 +10,23 @@
 
 class DemoScene : public SimpleLib::IOpenGLScene
 {
-    public:
-        DemoScene(void);
-        virtual ~DemoScene(void);
-        
-        void init(void);
-        void update(const float currTime);
-        void render(sf::Window& window);
-        void release(void);
-        
-        std::string getTitle(void);
-        
-    private:
-        void _createFromHeightMap(void);
-        void _createPyramid(void);  
-        
-    private:
-        VoxelChunk* _voxelChunk;
+	public:
+		DemoScene(void);
+		virtual ~DemoScene(void);
+		
+		void init(void);
+		void update(const float currTime);
+		void render(sf::Window& window);
+		void release(void);
+		
+		std::string getTitle(void);
+		
+	private:
+		void _createFromHeightMap(void);
+		void _createPyramid(void);	
+		
+	private:
+		VoxelChunk* _voxelChunk;
 };
 
 #endif
